d25784f23a62a8beb198405fae74d6373c6d6843
unknown <didiersangwa@gmail.com>
2022-06-03 01:46:16 +0200

    Joint functions

--- data.sql
+++ data.sql
@@ -1,27 +1,54 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-03-02', 0, true, 10.23);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, true, 8.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, false, 15.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-12-05', 5, true, 11.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-08-03', 0, false, -11.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantomn', '2021-15-11', 2, true, -5.7);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-02-04', 3, false, -12.13);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-12-06', 1, false, -45.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-07-06', 7, true, 20.4);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-13-10', 3, true, 17.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-14-05', 4, true, 22.0);
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (1, 'Agumon', '2020-02-03', 0, 'yes', 10.23 );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (2, 'Gabumon', '2018-11-15', 2, 'yes', 8.0 );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (3, 'Pikachu', '2021-01-07', 1, 'no', 15.04 );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES (4, 'Devimon', '2017-05-12', 5, 'yes', 11.0 );
 
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species = NULL;
-DELETE FROM animals;
-ROLLBACK BeforeDelete;
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (5, 'Charmander', '2020-02-08', 0, 'no', 11.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (6, 'Plantmon', '2021-11-15', 2, 'yes', 5.7, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (7, 'Squirtle', '1993-04-02', 3, 'no', 12.13, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (8, 'Angemon', '2005-06-12', 1, 'yes', 45.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (9, 'Boarmon', '2005-06-07', 7, 'yes', 20.4, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (10, 'Blossom', '1998-10-13', 3, 'yes', 17.0, '' );
+INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg,species) VALUES (11, 'Ditto', '2022-05-14', 4, 'yes', 22.0, '' );
 
-/*Delete all animals born after Jan 1st, 2022.*/
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+/* Insert data into the owners table */
+INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34);
+INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age) VALUES('Bob', 45);
+INSERT INTO owners(full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
+INSERT INTO owners(full_name, age) VALUES('Jodie Whittaker', 38);
 
-/*Update all animals' weight to be their weight multiplied by -1.*/
-UPDATE animals SET weight_kg = (weight_kg * (-1));
+/* Insert data into the species table */
+INSERT INTO species(name) Values('Pokemon');
+INSERT INTO species(name) Values('Digimon');
 
-/*Update all animals' weights that are negative to be their weight multiplied by -1.*/
-UPDATE animals SET weight_kg = (weight_kg * (-1)) WHERE weight_kg LIKE '%-';
+/* Begin transaction */
+BEGIN;
+
+/* If the name ends in "mon" it will be Digimon */
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+
+/* All other animals are Pokemon */
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+COMMIT;
+
+/* Begin transaction */
+BEGIN;
+
+/* Modify your inserted animals to include owner information (owner_id)*/
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name LIKE 'Gabumon';
+UPDATE animals SET owner_id = 2 WHERE name LIKE 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name LIKE 'Devimon';
+UPDATE animals SET owner_id = 3 WHERE name LIKE 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Charmander';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Squirtle';
+UPDATE animals SET owner_id = 4 WHERE name LIKE 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name LIKE 'Angemon';
+UPDATE animals SET owner_id = 5 WHERE name LIKE 'Boarman';
+
+COMMIT;
